c7da78d1e6d917e674de8a37237af99b872718c0
Sharon Varghese <kmit.sharonvarghese@gmail.com>
2020-11-03 20:23:26 +0530

    KMeans and Gaussian for fma and spotify

---
+++ Gaussian_fma_spotify.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+
+train = pd.read_csv("spotify_fma_merged.csv")
+train_modified = train.drop(['track_id','analysis_url','track_href','uri','id','type','track_title','genre','set_size','set_type','album'],axis=1)
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(train_modified)
+pca = PCA(n_components = 2)
+principal_components = pca.fit_transform(scaled_data)
+model = GaussianMixture(n_components = 5)
+y_train = model.fit_predict(principal_components)
+frame = pd.DataFrame(principal_components)
+frame['cluster'] = y_train
+color = ['blue','green','red','yellow','black']
+for k in range(0,5):
+  data = frame[frame["cluster"]==k]
+  plt.scatter(data[0], data[1], c=color[k])
