937c0da3fa046d52acbde949a03d649618168dad
Sharon Varghese <kmit.sharonvarghese@gmail.com>
2020-11-04 22:24:43 +0530

    Update KMeans_fma_spotify.py

--- KMeans_fma_spotify.py
+++ KMeans_fma_spotify.py
@@ -14,9 +14,12 @@ model = KMeans(n_clusters=5)
 y_train = model.fit_predict(principal_components)
 frame = pd.DataFrame(principal_components)
 frame['cluster'] = y_train
-color = ['blue','green','red','yellow','black']
+plt.figure(figsize=(10,10))
+color = ['blue','green','red','yellow','cyan']
 for k in range(0,5):
   data = frame[frame["cluster"]==k]
-  plt.scatter(data[0], data[1], c=color[k])
+  plt.scatter(data[0], data[1], c=color[k],s=400)
+for myint in range(0,len(frame)):
+  plt.annotate(str(myint+1),(frame[0].tolist()[myint],frame[1].tolist()[myint]))
 centers = model.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
